bbcace585afd1815dac0021927f01baefefffe99
TF-Joynic <wkf.joynic@gmail.com>
2019-05-20 20:01:59 +0800

    [MODIFY]logback conf

--- src/main/java/indi/joynic/joodoo/web/PortalServlet.java
+++ src/main/java/indi/joynic/joodoo/web/PortalServlet.java
@@ -1,37 +1,52 @@
 package indi.joynic.joodoo.web;
 
+import indi.joynic.joodoo.web.http.UrlUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Portal of the Web Application
+ *
  */
+/*@WebServlet(name = "portalServlet", urlPatterns = "/**", loadOnStartup = 1,
+        initParams = { @WebInitParam(name = "key1", value = "val1") },
+        asyncSupported = true
+)*/
 public class PortalServlet extends HttpServlet {
 
-    @Override
+    private static final Logger logger = LoggerFactory.getLogger(PortalServlet.class);
+
+    /*@Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
+            throws ServletException, IOException
+    {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-        writer.println("<html>");
-        writer.println("<head>");
-        writer.println("<title>Joodoo Web</title>");
-        writer.println("</head>");
-        writer.println("<body bgcolor=white>");
-        writer.println("<table border=\"0\">");
-        writer.println("<tr>");
-        writer.println("<td>");
-        writer.println("next gen web framework");
-        writer.println("</td>");
-        writer.println("</tr>");
-        writer.println("</table>");
-        writer.println("</body>");
-        writer.println("</html>");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }*/
 
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        logger.info("service!");
+
+        // path 责任链
+        //
+
+        // DISPATCH!
+        resp.getWriter().println(UrlUtil.extractRequestUri(req));
     }
 }
